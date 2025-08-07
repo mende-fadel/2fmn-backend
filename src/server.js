386bba2712faa20ed6f59bb6a2de56eef4f0a734
mende <fadelmende97@icloud.com>
2025-08-07 10:14:21 +0200

    modification  server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/stats", statsRoutes);
 // Connexion à MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connecté à MongoDB");
     app.listen(5000, () => console.log("🚀 Serveur backend sur http://localhost:5000"));
